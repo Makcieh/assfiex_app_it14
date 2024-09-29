b3cca5340c3f8ee95c88c878888cd493c2d9e38b
Makcieh <maxxfrio123@gmail.com>
2024-09-29 04:46:02 -0700

    updated on 9-29

---
+++ lib/pages/employees_pages/employee_fill.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class EmployeeFill extends StatelessWidget {
+  const EmployeeFill({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topLeft,
+              colors: [
+                Color.fromARGB(255, 100, 206, 255),
+                Color.fromARGB(255, 16, 133, 229)
+              ],
+            ),
+          ),
+        ),
+        title: const Text(
+          'Add Employees',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/employees_pages/employees_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class EmployeesPage extends StatelessWidget {
+  const EmployeesPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.bottomLeft,
+              end: Alignment.topLeft,
+              colors: [
+                Color.fromARGB(255, 100, 206, 255),
+                Color.fromARGB(255, 16, 133, 229)
+              ],
+            ),
+          ),
+        ),
+        title: const Text(
+          'Employees',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/addemployee');
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor:
+                      const Color.fromARGB(255, 61, 102, 135), // Text color
+                  padding: const EdgeInsets.symmetric(
+                      horizontal: 30, vertical: 12), // Button size and padding
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8), // Rounded corners
+                  ),
+                  elevation: 2, // Elevation to match the "raised" effect
+                ),
+                child: const Text(
+                  'ADD EMPLOYEES',
+                  style: TextStyle(
+                    letterSpacing: 3,
+                  ),
+                )),
+            Container(),
+          ],
+        ),
+      ),
+    );
+  }
+}

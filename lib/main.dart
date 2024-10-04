2f03fe6296cbe83d09918ccfc96243136e07e74b
Makcieh <maxxfrio123@gmail.com>
2024-10-04 06:44:32 -0700

    Updated on Oct-4 21:43

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
-import 'package:assfiex_app_it14/components/bottom_nav.dart';
-import 'package:assfiex_app_it14/pages/employees_pages/employee_fill.dart';
-import 'package:assfiex_app_it14/pages/employees_pages/employees_page.dart';
-import 'package:assfiex_app_it14/pages/requestpages/all_request.dart';
-import 'package:assfiex_app_it14/pages/requestpages/request_page.dart';
-import 'package:assfiex_app_it14/pages/Time_Avail_page.dart';
-import 'package:assfiex_app_it14/pages/auth_page.dart';
-import 'package:assfiex_app_it14/pages/createschedpages/create_sched_page.dart';
-import 'package:assfiex_app_it14/pages/menu_page.dart';
+import 'package:assfiex_app_it14/employee_side/pages/Time_Avail_page.dart';
+import 'package:assfiex_app_it14/employee_side/pages/components/bottom_nav.dart';
+import 'package:assfiex_app_it14/employee_side/pages/createschedpages/create_sched_page.dart';
+import 'package:assfiex_app_it14/employee_side/pages/employees_pages/employees_page.dart';
+import 'package:assfiex_app_it14/employee_side/pages/menu_page.dart';
+import 'package:assfiex_app_it14/employee_side/pages/requestpages/all_request.dart';
+import 'package:assfiex_app_it14/employee_side/pages/requestpages/request_page.dart';
+import 'package:assfiex_app_it14/manager_side/components/bottom_nav.dart';
+import 'package:assfiex_app_it14/manager_side/pages/employees_pages/employee_fill.dart';
+import 'package:assfiex_app_it14/manager_side/pages/employees_pages/employees_page.dart';
+import 'package:assfiex_app_it14/manager_side/pages/requestpages/all_request.dart';
+import 'package:assfiex_app_it14/manager_side/pages/requestpages/request_page.dart';
+import 'package:assfiex_app_it14/manager_side/pages/Time_Avail_page.dart';
+import 'package:assfiex_app_it14/manager_side/pages/auth_page.dart';
+import 'package:assfiex_app_it14/manager_side/pages/createschedpages/create_sched_page.dart';
+import 'package:assfiex_app_it14/manager_side/pages/menu_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -26,14 +33,28 @@ class MyApp extends StatelessWidget {
         home: const AuthPage(),
         routes: {
           //routes in all pages
+
+          //Manger Side Routes
           '/menupage': (context) => const MenuPage(),
-          '/createschedpage': (context) => CreateSchedPage(),
+          '/createschedpage': (context) => const CreateSchedPage(),
           '/timeavailpage': (context) => const TimeAvailPage(),
           '/requestpage': (context) => const RequestPage(),
           '/employeepage': (context) => const EmployeesPage(),
           '/try': (context) => const BottomNav(),
           '/allrequest': (context) => const AllRequest(),
-          '/addemploye': (context) => const EmployeeFill()
+          '/addemploye': (context) => const EmployeeFill(),
+
+          //Employee Side Routes
+          '/employee_menupage': (context) => const Employee_MenuPage(),
+          '/employee_createschedpage': (context) =>
+              const Employee_CreateSchedPage(),
+          '/employee_timeavailpage': (context) =>
+              const Employee_TimeAvailPage(),
+          // '/employee_requestpage': (context) => const Employee_RequestPage(),
+          '/employee_employeepage': (context) => const See_EmployeesPage(),
+          '/employee_try': (context) => const Employee_BottomNav(),
+          '/employee_allrequest': (context) => const Employee_AllRequest(),
+          '/employee_addemploye': (context) => const See_EmployeesPage()
         });
   }
 }
--- lib/pages/firestore.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class FirestoreService {
-  //get collection of request
-  final CollectionReference request =
-      FirebaseFirestore.instance.collection('requestleave');
-
-  //create: add a new data
-}
